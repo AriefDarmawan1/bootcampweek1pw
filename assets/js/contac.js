f1dafc3573ac0f0e2b1a88e7ac089f6258e54eaa
arief darmawan <ariefdrmawan@gmail.com>
2024-08-31 23:40:48 +0700

    update assets

---
+++ assets/js/contac.js
@@ -0,0 +1,35 @@
+function handlesubmit(event) {
+  event.preventDefault();
+  const inputname = document.getElementById("name").value;
+  const inputemail = document.getElementById("email").value;
+  const inputPhonenumber = document.getElementById("Phonenumber").value;
+  const inputsubject = document.getElementById("subject").value;
+  const inputpesan = document.getElementById("pesan").value;
+
+  if (inputname.length == 0) {
+    return alert(`Nama Gak Boleh Kosong BOSS`);
+  } else if (!inputemail.length) {
+    return alert(`Emailnya Kosong Nih`);
+  } else if (!inputPhonenumber.length) {
+    return alert(`Nomornya kosong boss`);
+  } else if (!inputsubject.length) {
+    return `Pilih dulu Mau kelas apa boss`;
+  } else if (!inputpesan.length) {
+    return `kirim Pesan apa boss`;
+  }
+
+  const link = document.createElement("a");
+  link.href = `mailto:${inputemail}subject: ${inputsubject}&body=name: ${inputname}\nNomorhp: ${inputPhonenumber}\npesan: ${inputpesan}`;
+
+  link.click();
+
+  const contac = {
+    name: inputname,
+    email: inputemail,
+    phoneNumber: inputPhoneNumber,
+    subject: inputSubject,
+    message: inputMessage,
+  };
+
+  console.log(contac);
+}
